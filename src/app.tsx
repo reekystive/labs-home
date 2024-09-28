1963713bd59379741d7e116ba49e97d3102ada77
ReekyStive <reekystive@gmail.com>
2024-09-29 05:23:19 +0800

    feat: ✨ change vh to dvh

--- src/app.tsx
+++ src/app.tsx
@@ -7,7 +7,7 @@ import { useSwitchStore } from './store/switch.ts';
 export const App: FC = () => {
   const { showList } = useSwitchStore();
   return (
-    <div className="flex h-screen flex-row items-center justify-center gap-8 overflow-hidden">
+    <div className="flex h-dvh flex-row items-center justify-center gap-8 overflow-hidden">
       {showList ? <SiteList sites={sites} /> : null}
       <Numpad />
     </div>
